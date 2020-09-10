82aece8aceb5f2a84a15e7d90aaa5d5f792f848f
Дмитрий Страхов <dimka_strahov@mail.ru>
2020-09-11 02:17:03 +0300

    Реализовано 1 задание

---
+++ stcgraphicpoints.cpp
@@ -0,0 +1,6 @@
+#include "stcgraphicpoints.h"
+
+stcGraphicPoints::stcGraphicPoints()
+{
+
+}
---
+++ stcgraphicpoints.h
@@ -0,0 +1,11 @@
+#ifndef STCGRAPHICPOINTS_H
+#define STCGRAPHICPOINTS_H
+
+
+class stcGraphicPoints : public QGraphicsView
+{
+public:
+	stcGraphicPoints();
+};
+
+#endif // STCGRAPHICPOINTS_H
